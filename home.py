65fe921754405f9d84cf71d0bbfd151453a53472
MandyB0716 <ambrown17s@semo.edu>
2025-12-14 20:49:41 -0600

    updated chat history view and requirements
    Signed-off-by: MandyB0716 <ambrown17s@semo.edu>

--- home.py
+++ home.py
@@ -19,3 +19,36 @@ with st.sidebar:
                 st.success("Text uploaded successfully!")
         else:
             st.error("Please enter text before uploading.")
+
+st.header("Ask Chat anything from our Knowledge Base")
+
+#Intialize the whole message history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+#Display the chat history
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+#Handle user input
+prompt = st.chat_input("Ask any question to the chat")
+if prompt:
+    st.chat_message("user").markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # Generate rag response in here
+    with st.chat_message("ROBOT"):
+        with st.spinner("THINKINGSS"):
+            response_data = rag.get_rag_response(prompt)
+            answer = response_data["answer"]
+            sources = response_data["sources"]
+
+            st.markdown(answer)
+
+            # show sources in a expander
+            with st.expander("Sources"):
+                for i, source in enumerate(sources):
+                    st.markdown(f"**Source {i+1}:** {source.page_content}")
+            # Append the response to the message history
+            st.session_state.messages.append({"role": "ROBOT", "content": answer})
